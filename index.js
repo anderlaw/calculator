e733b90122d3c325c81d0e508826d7566c59f6a7
hajerbin <974088545@qq.com>
2020-04-27 11:53:50 +0800

    replace file

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-//
-//
-var outputTxt = 0;//默认为0
 var inputTxt = ''
-$('[data-key]').on('click',(e)=>{
-  //AC，equal需要特殊处理
+var outputTxt = ''
+//add event listening
+$('.line-item').on('click',e=>{
+  // console.log('here')
+  playSoud()
   var key = e.target.dataset.key
-  if(key === 'AC'){
-    //clear All
-    outputTxt = 0;
-    inputTxt = '';
-  }else if(key === 'equal'){
-
-    try {
-      outputTxt = eval(inputTxt)
-    } catch (error) {
-      outputTxt = '错误'
-    } 
-  }else{
-    inputTxt += key
+  console.log(key)
+  switch(key){
+    case '=':
+      try{
+        outputTxt = eval(inputTxt)
+      }catch(e){
+        outputTxt = 'error'
+      }
+      break;
+    case 'AC':
+      //clear all inputs and outputs
+      inputTxt = ''
+      outputTxt = '0'
+      break;
+    default:
+      inputTxt += key
   }
-  play()
+  //update the view
   updateView()
-
 })
-function updateView(){
-  $('.output-box').text(outputTxt)
-  $('.input-box').text(inputTxt)
+function playSoud(){
+  $('audio')[0].play()
 }
-function play(){
-  $('#tips')[0].play();
+function updateView(){
+  $('.input-box').html(inputTxt)
+  $('.output-box').html(outputTxt)
 }
